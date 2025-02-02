ad72294f4e4fb7692c7a71c2f48f0e7cf0708e1e
Davey <125982535+Milave-kun@users.noreply.github.com>
2025-02-02 15:51:31 +0800

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Default.aspx.vb" Inherits="Login_Form._Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="styles/StyleSheet.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="width: 300px; margin: 0 auto; padding-top: 50px;">
+            <h2>Registration Form</h2>
+
+            <div> 
+                <label for="txtUsername">Username : </label>
+                <asp:TextBox ID="txtUsername" runat="server" Width="200px" RequiredFieldValidator1="true"></asp:TextBox>
+            </div>
+
+            <div>
+                <label for="txtPassword">Password : </label>
+                <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" Width="200px" RequiredFieldValidator2="true"></asp:TextBox>
+            </div>
+
+            <div>
+                <label for="txtDisplayName">Display Name : </label>
+                <asp:TextBox ID="txtDisplayName" runat="server" Width="200px" RequiredFieldValidator3="true"> </asp:TextBox>
+            </div>
+
+            <div>
+                <label for="ddlUserType">User Type : </label>
+                <asp:DropDownList ID="ddlUserType" runat="server" Width="200px">
+                <asp:ListItem Text="Student" Value="1"></asp:ListItem>
+                <asp:ListItem Text="Admin" Value="2"></asp:ListItem>
+                </asp:DropDownList>
+            </div>
+
+            <div>
+                <asp:Button ID="btnSubmit" runat="server" Text="Register" />
+            </div>
+        </div>
+    </form>
+
+            <center>
+                <asp:Table ID="Table1" runat="server">
+                </asp:Table>
+            </center>
+</body>
+</html>
---
+++ Default.aspx.vb
@@ -0,0 +1,66 @@
+﻿Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Dim CONNECT As New Connection()
+
+    Protected Sub btnSubmit_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles btnSubmit.Click
+        Register()
+    End Sub
+
+    Public Sub Register()
+        Dim username = txtUsername.Text
+        Dim password = txtPassword.Text
+        Dim display_name = txtDisplayName.Text
+        Dim user_type = ddlUserType.SelectedValue
+
+        Dim query = "INSERT INTO users (username, password, display_name, user_type) VALUES (@username, @password, @display_name, @user_type)"
+
+        CONNECT.AddParam("@username", username)
+        CONNECT.AddParam("@password", password)
+        CONNECT.AddParam("@display_name", display_name)
+        CONNECT.AddParam("@user_type", user_type)
+
+        Dim insert = CONNECT.Query(query)
+
+        If insert Then
+            Dim script As String = "alert('Successfully Register!');"
+            ClientScript.RegisterStartupScript(Me.GetType(), "alertMessage", script, True)
+        Else
+            Dim script As String = "alert('Failed to Register!');"
+            ClientScript.RegisterStartupScript(Me.GetType(), "alertMessage", script, True)
+        End If
+
+        ViewTable()
+    End Sub
+
+    Public Sub ViewTable()
+        Dim query = "SELECT * FROM users"
+
+        CONNECT.Query(query)
+
+        Table1.Rows.Clear()
+
+        Dim headerRow As New TableHeaderRow()
+        headerRow.Cells.Add(New TableHeaderCell() With {.Text = "Username"})
+        headerRow.Cells.Add(New TableHeaderCell() With {.Text = "Password"})
+        headerRow.Cells.Add(New TableHeaderCell() With {.Text = "Display Name"})
+        headerRow.Cells.Add(New TableHeaderCell() With {.Text = "User Type"})
+        Table1.Rows.Add(headerRow)
+
+        For Each row As DataRow In CONNECT.Data.Tables(0).Rows
+            Dim tableRow As New TableRow()
+
+            tableRow.Cells.Add(New TableCell() With {.Text = row("username").ToString()})
+            tableRow.Cells.Add(New TableCell() With {.Text = row("password").ToString()})
+            tableRow.Cells.Add(New TableCell() With {.Text = row("display_name").ToString()})
+            tableRow.Cells.Add(New TableCell() With {.Text = row("user_type").ToString()})
+
+            Table1.Rows.Add(tableRow)
+        Next
+    End Sub
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ViewTable()
+    End Sub
+
+End Class
